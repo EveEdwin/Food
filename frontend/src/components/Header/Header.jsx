65b2f17d36c826b3ee502e7a2b288a288d6f2a19
Mohit <me.mohitsharma14@gmail.com>
2024-07-02 06:30:57 +0530

    40% work done

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+
+      <div className="header">
+<div className="header-contents">
+    <h2>
+        Order your facvourite food here
+    </h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro reiciendis, ratione modi sit nesciunt corporis. Ad enim facilis perspiciatis, vitae quasi libero molestias earum culpa unde id expedita veniam. Sequi?</p>
+    <button>Menu</button>
+</div>
+      </div>
+  )
+}
+
+export default Header
