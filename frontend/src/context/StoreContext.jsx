7dc1f5345045c9aa2cc1231bcedff1d04940e14d
Mohit <me.mohitsharma14@gmail.com>
2024-07-14 22:44:30 +0530

    Minor change

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -19,6 +19,7 @@ const StoreContextProvider =(props) => {
     const removeFromCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
         
+        
     }
 
     useEffect(()=>{
